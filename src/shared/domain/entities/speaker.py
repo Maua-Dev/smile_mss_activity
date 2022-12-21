59fab6d6c8d13a768fae30fe737b0f1227f7207a
Vitor Soller <vgsoller@gmail.com>
2022-12-20 21:05:37 -0300

    add __repr__ and __eq__ in speaker.py

--- src/shared/domain/entities/speaker.py
+++ src/shared/domain/entities/speaker.py
@@ -31,3 +31,9 @@ class Speaker:
             return False
 
         return True
+
+    def __repr__(self):
+        return f"Speaker(name={self.name}, bio={self.bio}, company={self.company})"
+
+    def __eq__(self, other):
+        return self.name == other.name and self.bio == other.bio and self.company == other.company
