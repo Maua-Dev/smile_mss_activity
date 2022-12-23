56b18410edcc54f9660e3c8aaef354ff02eaf28b
Vitor Soller <vgsoller@gmail.com>
2022-12-23 10:44:22 -0300

    fix tests in test_speaker.py

--- tests/shared/domain/entities/test_speaker.py
+++ tests/shared/domain/entities/test_speaker.py
@@ -1,34 +1,38 @@
 import pytest
-#apagar
 from src.shared.domain.entities.speaker import Speaker
 from src.shared.helpers.errors.domain_errors import EntityError
 
 
 class Test_Speaker:
     def test_speaker(self):
-        Speaker(name="Briqz", bio="Daora", company="Microsoft")
+        speaker = Speaker(name="Briqz", bio="Daora", company="Microsoft")
+
+        assert type(speaker) == Speaker
+        assert speaker.name == "Briqz"
+        assert speaker.bio == "Daora"
+        assert speaker.company == "Microsoft"
 
 
     def test_speaker_invalid_name(self):
         with pytest.raises(EntityError):
-            Speaker(name="B", bio="Daora", company="Microsoft")
+            speaker = Speaker(name="B", bio="Daora", company="Microsoft")
 
     def test_speaker_invalid_bio(self):
         with pytest.raises(EntityError):
-            Speaker(name="Briqz", bio=1, company="Microsoft")
+            speaker = Speaker(name="Briqz", bio=1, company="Microsoft")
 
     def test_speaker_invalid_company(self):
         with pytest.raises(EntityError):
-            Speaker(name="Briqz", bio="Daora", company=1)
+            speaker = Speaker(name="Briqz", bio="Daora", company=1)
 
     def test_speaker_invalid_name_none(self):
         with pytest.raises(EntityError):
-            Speaker(name=None, bio="Daora", company="Microsoft")
+            speaker = Speaker(name=None, bio="Daora", company="Microsoft")
 
     def test_speaker_invalid_bio_none(self):
         with pytest.raises(EntityError):
-            Speaker(name="Briqz", bio=None, company="Microsoft")
+            speaker = Speaker(name="Briqz", bio=None, company="Microsoft")
 
     def test_speaker_invalid_company_none(self):
         with pytest.raises(EntityError):
-            Speaker(name="Briqz", bio="Daora", company=None)
+            speaker = Speaker(name="Briqz", bio="Daora", company=None)
