a27ffe7b8b136249aac76f55de8d5d768edd9a5b
Vitor Soller <vgsoller@gmail.com>
2023-01-03 13:11:33 -0300

    add 404 and 400 in test_get_enrollment_presenter.py

--- tests/modules/get_enrollment/app/test_get_enrollment_presenter.py
+++ tests/modules/get_enrollment/app/test_get_enrollment_presenter.py
@@ -1,3 +1,5 @@
+import json
+
 from src.modules.get_enrollment.app.get_enrollment_presenter import lambda_handler
 
 
@@ -59,3 +61,118 @@ class Test_GetEnrollmentPresenter:
 
         response = lambda_handler(event, None)
         assert response["statusCode"] == 200
+
+    def test_get_enrollment_presenter_404(self):
+            event = {
+                "version": "2.0",
+                "routeKey": "$default",
+                "rawPath": "/my/path",
+                "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
+                "cookies": [
+                    "cookie1",
+                    "cookie2"
+                ],
+                "headers": {
+                    "header1": "value1",
+                    "header2": "value1,value2"
+                },
+                "queryStringParameters": {
+                    'user_id': "db43",
+                    'code': "ECM2341"
+                },
+                "requestContext": {
+                    "accountId": "123456789012",
+                    "apiId": "<urlid>",
+                    "authentication": None,
+                    "authorizer": {
+                        "iam": {
+                            "accessKey": "AKIA...",
+                            "accountId": "111122223333",
+                            "callerId": "AIDA...",
+                            "cognitoIdentity": None,
+                            "principalOrgId": None,
+                            "userArn": "arn:aws:iam::111122223333:user/example-user",
+                            "userId": "AIDA..."
+                        }
+                    },
+                    "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
+                    "domainPrefix": "<url-id>",
+                    "external_interfaces": {
+                        "method": "POST",
+                        "path": "/my/path",
+                        "protocol": "HTTP/1.1",
+                        "sourceIp": "123.123.123.123",
+                        "userAgent": "agent"
+                    },
+                    "requestId": "id",
+                    "routeKey": "$default",
+                    "stage": "$default",
+                    "time": "12/Mar/2020:19:03:58 +0000",
+                    "timeEpoch": 1583348638390
+                },
+                "body": "Hello from client!",
+                "pathParameters": None,
+                "isBase64Encoded": None,
+                "stageVariables": None
+            }
+
+            response = lambda_handler(event, None)
+            assert response["statusCode"] == 404
+            assert json.loads(response['body']) == 'No items found for enrollment'
+
+    def test_get_enrollment_presenter_400(self):
+        event = {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/my/path",
+            "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
+            "cookies": [
+                "cookie1",
+                "cookie2"
+            ],
+            "headers": {
+                "header1": "value1",
+                "header2": "value1,value2"
+            },
+            "queryStringParameters": {
+                'code': "ECM2341"
+            },
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": {
+                    "iam": {
+                        "accessKey": "AKIA...",
+                        "accountId": "111122223333",
+                        "callerId": "AIDA...",
+                        "cognitoIdentity": None,
+                        "principalOrgId": None,
+                        "userArn": "arn:aws:iam::111122223333:user/example-user",
+                        "userId": "AIDA..."
+                    }
+                },
+                "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "<url-id>",
+                "external_interfaces": {
+                    "method": "POST",
+                    "path": "/my/path",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent"
+                },
+                "requestId": "id",
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390
+            },
+            "body": "Hello from client!",
+            "pathParameters": None,
+            "isBase64Encoded": None,
+            "stageVariables": None
+        }
+
+        response = lambda_handler(event, None)
+        assert response["statusCode"] == 400
+        assert json.loads(response['body']) == "Field user_id is missing"
