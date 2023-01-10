12f2ca5c9827421eeb26a671af029ef3f9c9e40b
Vitor Soller <vgsoller@gmail.com>
2023-01-10 10:27:21 -0300

    add drop_activity_usecase.py with UT

---
+++ src/modules/drop_activity/app/drop_activity_usecase.py
@@ -0,0 +1,35 @@
+from src.shared.domain.entities.enrollment import Enrollment
+from src.shared.domain.entities.user import User
+from src.shared.domain.enums.enrollment_state_enum import ENROLLMENT_STATE
+from src.shared.domain.repositories.activity_repository_interface import IActivityRepository
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import NoItemsFound
+
+
+class DropActivityUsecase:
+    def __init__(self, repo: IActivityRepository):
+        self.repo = repo
+
+    def __call__(self, user_id: str, code: str) -> Enrollment:
+        if not User.validate_user_id(user_id):
+            raise EntityError('user_id')
+
+        if type(code) is not str:
+            raise EntityError('code')
+
+        activity = self.repo.get_activity(code)
+        if activity is None:
+            raise NoItemsFound('activity')
+        taken_slots = activity.taken_slots
+
+        enrollment = self.repo.update_enrollment(user_id=user_id, code=code, state=ENROLLMENT_STATE.DROPPED)
+
+        if taken_slots >= activity.total_slots:
+            activity, enrollments = self.repo.get_activity_with_enrollments(code)
+            in_queue_enrollments = list(filter(lambda enrollment: enrollment.state == ENROLLMENT_STATE.IN_QUEUE, enrollments))
+            if len(in_queue_enrollments) > 0:
+                in_queue_enrollments.sort(key=lambda enrollment: enrollment.date_subscribed)
+                enrollment = in_queue_enrollments[0]
+                updated_enrollment = self.repo.update_enrollment(user_id=enrollment.user.user_id, code=code, state=ENROLLMENT_STATE.ENROLLED)
+
+        return enrollment
---
+++ tests/modules/drop_activity/app/test_drop_activity_usecase.py
@@ -0,0 +1,52 @@
+import pytest
+
+from src.modules.drop_activity.app.drop_activity_usecase import DropActivityUsecase
+from src.shared.domain.enums.enrollment_state_enum import ENROLLMENT_STATE
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import NoItemsFound
+from src.shared.infra.repositories.activity_repository_mock import ActivityRepositoryMock
+
+
+class Test_DropActivityUsecase:
+    def test_drop_activity_usecase_no_queue(self):
+        repo = ActivityRepositoryMock()
+        usecase = DropActivityUsecase(repo)
+        taken_slots_before = repo.activities[1].taken_slots
+        dropped_enrollment = usecase("b16f", "ELET355")
+
+        assert repo.enrollments[7].state == ENROLLMENT_STATE.DROPPED
+        assert taken_slots_before - 1 == repo.activities[1].taken_slots
+
+    def test_drop_activity_usecase_with_queue(self):
+        repo = ActivityRepositoryMock()
+        usecase = DropActivityUsecase(repo)
+        taken_slots_before = repo.activities[1].taken_slots
+        dropped_enrollment = usecase("d7f1", "ECM2345")
+
+        assert repo.enrollments[2].state == ENROLLMENT_STATE.DROPPED
+        assert repo.enrollments[4].state == ENROLLMENT_STATE.ENROLLED
+        assert  taken_slots_before == repo.activities[1].taken_slots
+
+    def test_drop_activity_usecase_invalid_user_id(self):
+        repo = ActivityRepositoryMock()
+        usecase = DropActivityUsecase(repo)
+
+        with pytest.raises(EntityError):
+            dropped_enrollment = usecase("b16f1", "ELET355")
+
+
+    def test_drop_activity_usecase_invalid_code(self):
+        repo = ActivityRepositoryMock()
+        usecase = DropActivityUsecase(repo)
+
+        with pytest.raises(EntityError):
+            dropped_enrollment = usecase("b16f", 123)
+
+    def test_drop_activity_usecase_no_activity_found(self):
+        repo = ActivityRepositoryMock()
+        usecase = DropActivityUsecase(repo)
+
+        with pytest.raises(NoItemsFound):
+            dropped_enrollment = usecase("b16f", "CODIGO_INEXISTENTE")
+
+
