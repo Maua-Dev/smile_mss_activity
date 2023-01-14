170ce05ac29722c08c3cef3795960fcaf647d597
LucasRomanato0 <20.00313-7@maua.br>
2023-01-14 16:58:04 -0300

    presenter and test

---
+++ src/modules/enroll_activity/app/enroll_activity_presenter.py
@@ -0,0 +1,15 @@
+from src.modules.enroll_activity.app.enroll_activity_controller import EnrollActivityController
+from src.modules.enroll_activity.app.enroll_activity_usecase import EnrollActivityUsecase
+from src.shared.environments import Environments
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+
+repo = Environments.get_activity_repo()
+usecase = EnrollActivityUsecase(repo)
+controller = EnrollActivityController(usecase)
+
+def lambda_handler(event, context):
+       httpRequest = LambdaHttpRequest(data=event)
+       response = controller(httpRequest)
+       httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+
+       return httpResponse.toDict()
