7f5d2a56ad850bbe74edabb1105d4c648a91e99c
Vitor Soller <vgsoller@gmail.com>
2023-01-18 17:51:42 -0300

    add delete_activity_controller.py with UT

---
+++ src/modules/delete_activity/app/delete_activity_controller.py
@@ -0,0 +1,47 @@
+from .delete_activity_usecase import DeleteActivityUsecase
+from .delete_activity_viewmodel import DeleteActivityViewmodel
+from src.shared.helpers.errors.controller_errors import MissingParameters, WrongTypeParameter
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import NoItemsFound
+from src.shared.helpers.external_interfaces.external_interface import IRequest, IResponse
+from src.shared.helpers.external_interfaces.http_codes import OK, NotFound, BadRequest, InternalServerError
+
+
+class DeleteActivityController:
+
+    def __init__(self, usecase: DeleteActivityUsecase):
+        self.DeleteActivityUsecase = usecase
+
+    def __call__(self, request: IRequest) -> IResponse:
+        try:
+
+            if not request.data.get('code'):
+                raise MissingParameters('code')
+
+            enrollment = self.DeleteActivityUsecase(
+                code=request.data.get('code')
+            )
+
+            viewmodel = DeleteActivityViewmodel(enrollment)
+
+            return OK(viewmodel.to_dict())
+
+        except NoItemsFound as err:
+
+            return NotFound(body=err.message)
+
+        except MissingParameters as err:
+
+            return BadRequest(body=err.message)
+
+        except WrongTypeParameter as err:
+
+            return BadRequest(body=err.message)
+
+        except EntityError as err:
+
+            return BadRequest(body=err.message)
+
+        except Exception as err:
+
+            return InternalServerError(body=err.args[0])
