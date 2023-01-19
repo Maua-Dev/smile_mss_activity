28ef695f0ba8e06213e26e7e40568dfc0a53b9ca
Vitor Soller <vgsoller@gmail.com>
2023-01-19 13:21:58 -0300

    add get_all_activities_admin_presenter.py with UT

---
+++ src/modules/get_all_activities_admin/app/get_all_activities_admin_presenter.py
@@ -0,0 +1,16 @@
+from src.modules.get_all_activities_admin.app.get_all_activities_admin_controller import GetAllActivitiesAdminController
+from src.modules.get_all_activities_admin.app.get_all_activities_admin_usecase import GetAllActivitiesAdminUsecase
+from src.shared.environments import Environments
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+
+repo = Environments.get_activity_repo()()
+usecase = GetAllActivitiesAdminUsecase(repo)
+controller = GetAllActivitiesAdminController(usecase)
+
+
+def lambda_handler(event, context):
+    httpRequest = LambdaHttpRequest(data=event)
+    response = controller(httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+
+    return httpResponse.toDict()
