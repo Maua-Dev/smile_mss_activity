6464cb5ef2fc01c2f64a90bbf4e61725a4d391fe
Vitor Soller <vgsoller@gmail.com>
2023-01-20 11:20:44 -0300

    add type test in test_get_all_activities_admin_usecase.py

--- tests/modules/get_all_activities_admin/app/test_get_all_activities_admin_usecase.py
+++ tests/modules/get_all_activities_admin/app/test_get_all_activities_admin_usecase.py
@@ -1,4 +1,6 @@
 from src.modules.get_all_activities_admin.app.get_all_activities_admin_usecase import GetAllActivitiesAdminUsecase
+from src.shared.domain.entities.activity import Activity
+from src.shared.domain.entities.enrollment import Enrollment
 from src.shared.infra.repositories.activity_repository_mock import ActivityRepositoryMock
 
 
@@ -12,4 +14,6 @@ class Test_GetAllActivitiesAdminUsecase:
 
         assert type(all_activities_with_enrollments) == list
         assert type(all_activities_with_enrollments[0]) == tuple
+        assert type(all_activities_with_enrollments[0][0]) == Activity
+        assert all(all(type(enrollment) == Enrollment for enrollment in enrollments) for activity, enrollments in all_activities_with_enrollments)
         assert len(all_activities_with_enrollments) == len(repo.activities)
