f0a790e3e810faf9fc63b3b602a1be85f8445cd1
Vitor Soller <vgsoller@gmail.com>
2023-01-23 20:39:39 -0300

    update update_activity_usecase.py new logic

--- src/modules/update_activity/app/update_activity_usecase.py
+++ src/modules/update_activity/app/update_activity_usecase.py
@@ -8,84 +8,68 @@ from src.shared.domain.enums.delivery_model_enum import DELIVERY_MODEL
 from src.shared.domain.enums.role_enum import ROLE
 from src.shared.domain.repositories.activity_repository_interface import IActivityRepository
 from src.shared.helpers.errors.domain_errors import EntityError
-from src.shared.helpers.errors.usecase_errors import ForbiddenAction, NoItemsFound
+from src.shared.helpers.errors.usecase_errors import ForbiddenAction, NoItemsFound, DuplicatedItem
 
 
 class UpdateActivityUsecase:
-       def __init__(self, repo: IActivityRepository):
-              self.repo = repo
+    def __init__(self, repo: IActivityRepository):
+        self.repo = repo
 
-       def __call__(self, code: str, new_title: str = None, new_description: str = None, new_activity_type: str = None,
-                     new_is_extensive: bool = None, new_delivery_model: str = None,
-                     new_start_date: datetime.datetime = None, new_duration: int = None,new_responsible_professors: List[User] = None,
-                     new_speakers: List[Speaker] = None, new_total_slots: int = None, new_taken_slots: int = None,
-                     new_accepting_new_enrollments: bool = None,
-                     new_stop_accepting_new_enrollments_before: datetime.datetime = None, **kwargs) -> Activity:
-              
-              if type(code) is not str:
-                     raise EntityError('code')
-              
-              if new_title != None and type(new_title) is not str:
-                     raise EntityError('new_title')
+    def __call__(self, code: str, new_title: str, new_description: str, new_activity_type: ACTIVITY_TYPE,
+                 new_is_extensive: bool, new_delivery_model: DELIVERY_MODEL,
+                 new_start_date: datetime.datetime, new_duration: int, new_link: str, new_place: str,
+                 new_responsible_professors_user_id: List[str],
+                 new_speakers: List[Speaker], new_total_slots: int,
+                 new_accepting_new_enrollments: bool,
+                 new_stop_accepting_new_enrollments_before: datetime.datetime) -> Activity:
 
-              if new_description != None and type(new_description) is not str:
-                     raise EntityError('new_description')
+        if type(code) != str:
+            raise EntityError("code")
+        activity = self.repo.get_activity(code=code)
 
-              if new_activity_type != None:
-                     activity_types = [activity_type.value for activity_type in ACTIVITY_TYPE]
-                     if new_activity_type not in activity_types:
-                            raise EntityError('new_activity_type')
-                     new_activity_type = ACTIVITY_TYPE[new_activity_type]
+        if activity is None:
+            raise NoItemsFound("Activity")
 
-              if new_is_extensive != None and type(new_is_extensive) is not bool:
-                     raise EntityError('new_is_extensive')
+        if not all(type(user_id) == str for user_id in new_responsible_professors_user_id):
+            raise EntityError("responsible_professors")
 
-              if new_delivery_model != None:
-                     delivery_models = [delivery_model.value for delivery_model in DELIVERY_MODEL]
-                     if new_delivery_model not in delivery_models:
-                            raise EntityError('new_delivery_model')
-                     new_delivery_model = DELIVERY_MODEL[new_delivery_model]
-              
-              if new_start_date != None and type(new_start_date) is not datetime.datetime:
-                     raise EntityError('new_start_date')
+        new_responsible_professors = self.repo.get_users(new_responsible_professors_user_id)
 
-              if new_duration != None and type(new_duration) is not int:
-                     raise EntityError('new_duration')
-              
-              if new_responsible_professors != None and new_responsible_professors is List:
-                     for professor in new_responsible_professors:
-                            if type(professor) is not User:
-                                   raise EntityError('new_responsible_professors')
-                            if professor.role != ROLE.PROFESSOR:
-                                   raise ForbiddenAction('Professor')
-                            return professor
+        if len(new_responsible_professors) != len(new_responsible_professors_user_id):
+            raise NoItemsFound("responsible_professors")
 
-              if new_speakers != None and new_speakers is List:
-                     for speaker in new_speakers:
-                            if type(speaker) is not Speaker:
-                                   raise EntityError('new_speakers')
-                            return speaker
+        new_activity = Activity(
+            code=code,
+            title=new_title,
+            description=new_description,
+            activity_type=new_activity_type,
+            is_extensive=new_is_extensive,
+            delivery_model=new_delivery_model,
+            start_date=new_start_date,
+            duration=new_duration,
+            link=new_link,
+            place=new_place,
+            responsible_professors=new_responsible_professors,
+            speakers=new_speakers,
+            total_slots=new_total_slots,
+            taken_slots=activity.taken_slots,
+            accepting_new_enrollments=new_accepting_new_enrollments,
+            stop_accepting_new_enrollments_before=new_stop_accepting_new_enrollments_before
+        )
 
-              if new_total_slots != None and type(new_total_slots) is not int:
-                     raise EntityError('new_total_slots')
-
-              if new_taken_slots != None and type(new_taken_slots) is not int:
-                     raise EntityError('new_taken_slots')
-
-              if new_accepting_new_enrollments != None and type(new_accepting_new_enrollments) is not bool:
-                     raise EntityError('new_accepting_new_enrollments')
-
-              if new_stop_accepting_new_enrollments_before != None and type(new_stop_accepting_new_enrollments_before) is not datetime.datetime:
-                     raise EntityError('new_stop_accepting_new_enrollments_before')
-
-              activity = self.repo.get_activity(code)
-              if activity is None:
-                     raise NoItemsFound('Activity')
-
-              return self.repo.update_activity(code, new_title, new_description, new_activity_type,
-                     new_is_extensive, new_delivery_model,
-                     new_start_date, new_duration,new_responsible_professors,
-                     new_speakers, new_total_slots, new_taken_slots,
-                     new_accepting_new_enrollments,
-                     new_stop_accepting_new_enrollments_before)
-
+        return self.repo.update_activity(code=code,
+                                         new_title=new_title,
+                                         new_description=new_description,
+                                         new_activity_type=new_activity_type,
+                                         new_is_extensive=new_is_extensive,
+                                         new_delivery_model=new_delivery_model,
+                                         new_start_date=new_start_date,
+                                         new_duration=new_duration,
+                                         new_link=new_link,
+                                         new_place=new_place,
+                                         new_responsible_professors=new_responsible_professors,
+                                         new_speakers=new_speakers,
+                                         new_total_slots=new_total_slots,
+                                         new_taken_slots=activity.taken_slots,
+                                         new_accepting_new_enrollments=new_accepting_new_enrollments,
+                                         new_stop_accepting_new_enrollments_before=new_stop_accepting_new_enrollments_before)
--- tests/modules/update_activity/app/test_update_activity_usecase.py
+++ tests/modules/update_activity/app/test_update_activity_usecase.py
@@ -3,142 +3,189 @@ import pytest
 
 from src.modules.update_activity.app.update_activity_usecase import UpdateActivityUsecase
 from src.shared.domain.entities.activity import Activity
+from src.shared.domain.entities.speaker import Speaker
 from src.shared.domain.enums.activity_type_enum import ACTIVITY_TYPE
 from src.shared.domain.enums.delivery_model_enum import DELIVERY_MODEL
 from src.shared.helpers.errors.usecase_errors import ForbiddenAction, NoItemsFound
 from src.shared.infra.repositories.activity_repository_mock import ActivityRepositoryMock
 from src.shared.helpers.errors.domain_errors import EntityError
 
+
 class Test_UpdateActivityUsecase:
 
-       def test_update_activity(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
-              update_activity = usecase(code=repo.activities[0].code, new_description='nova descricao')
+    def test_update_activity(self):
+        repo = ActivityRepositoryMock()
+        usecase = UpdateActivityUsecase(repo)
+        update_activity = usecase(code=repo.activities[0].code, new_title="NOVO TITULO", new_description='nova descricao',
+                                  new_activity_type=ACTIVITY_TYPE.LECTURES,
+                                  new_is_extensive=True,
+                                  new_delivery_model=DELIVERY_MODEL.ONLINE,
+                                  new_start_date=datetime.datetime(2021, 9, 1, 0, 0), new_duration=15,
+                                  new_link="https://www.youtube.com/watch?v=1q2w3e4r5t6y7u8i9o0p",
+                                  new_place="Sala 1",
+                                  new_responsible_professors_user_id=[repo.users[2].user_id, repo.users[11].user_id],
+                                  new_speakers=[
+                                      Speaker(
+                                          name="Fulano de Tal",
+                                          bio="Fulano de Tal é um professor de Engenharia de Software",
+                                          company="Universidade Federal de Fulano de tal",
+                                      )
+                                  ], new_total_slots=100,
+                                  new_accepting_new_enrollments=True,
+                                  new_stop_accepting_new_enrollments_before=None)
 
-              assert type(update_activity) == Activity
-              assert repo.activities[0].description == update_activity.description
+        assert type(update_activity) == Activity
 
-       def test_update_activity_enum(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
-              update_activity = usecase(code=repo.activities[0].code, new_activity_type="LECTURES", new_delivery_model="HYBRID")
+        assert repo.activities[0].title == update_activity.title
+        assert repo.activities[0].description == update_activity.description
+        assert repo.activities[0].activity_type == update_activity.activity_type
+        assert repo.activities[0].is_extensive == update_activity.is_extensive
+        assert repo.activities[0].delivery_model == update_activity.delivery_model
+        assert repo.activities[0].start_date == update_activity.start_date
+        assert repo.activities[0].duration == update_activity.duration
+        assert repo.activities[0].link == update_activity.link
+        assert repo.activities[0].place == update_activity.place
+        assert repo.activities[0].responsible_professors == update_activity.responsible_professors
+        assert repo.activities[0].speakers == update_activity.speakers
+        assert repo.activities[0].total_slots == update_activity.total_slots
+        assert repo.activities[0].accepting_new_enrollments == update_activity.accepting_new_enrollments
+        assert repo.activities[0].stop_accepting_new_enrollments_before == update_activity.stop_accepting_new_enrollments_before
 
-              assert type(update_activity) == Activity
-              assert repo.activities[0].activity_type == update_activity.activity_type
-              assert repo.activities[0].delivery_model == update_activity.delivery_model
 
-       def test_update_activity_title_taken_slots(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
-              update_activity = usecase(code=repo.activities[0].code, new_title='NOVO TITULO', new_taken_slots=40)
 
-              assert type(update_activity) == Activity
-              assert repo.activities[0].title == update_activity.title
-              assert repo.activities[0].taken_slots == update_activity.taken_slots
+    def test_update_activity_invalid_enum(self):
+        repo = ActivityRepositoryMock()
+        usecase = UpdateActivityUsecase(repo)
+        with pytest.raises(EntityError):
+            update_activity = usecase(code=repo.activities[0].code, new_title="NOVO TITULO", new_description='nova descricao',
+                                  new_activity_type="LECTURES",
+                                  new_is_extensive=True,
+                                  new_delivery_model=DELIVERY_MODEL.ONLINE,
+                                  new_start_date=datetime.datetime(2021, 9, 1, 0, 0), new_duration=15,
+                                  new_link="https://www.youtube.com/watch?v=1q2w3e4r5t6y7u8i9o0p",
+                                  new_place="Sala 1",
+                                  new_responsible_professors_user_id=[repo.users[2].user_id, repo.users[11].user_id],
+                                  new_speakers=[
+                                      Speaker(
+                                          name="Fulano de Tal",
+                                          bio="Fulano de Tal é um professor de Engenharia de Software",
+                                          company="Universidade Federal de Fulano de tal",
+                                      )
+                                  ], new_total_slots=100,
+                                  new_accepting_new_enrollments=True,
+                                  new_stop_accepting_new_enrollments_before=None)
 
-       def test_update_activity_invalid_code(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
+    def test_update_activity_invalid_code(self):
+        repo = ActivityRepositoryMock()
+        usecase = UpdateActivityUsecase(repo)
 
-              with pytest.raises(EntityError):
-                     update_activity = usecase(555)
+        with pytest.raises(EntityError):
+            update_activity = usecase(code=555, new_title="NOVO TITULO", new_description='nova descricao',
+                                  new_activity_type=ACTIVITY_TYPE.LECTURES,
+                                  new_is_extensive=True,
+                                  new_delivery_model=DELIVERY_MODEL.ONLINE,
+                                  new_start_date=datetime.datetime(2021, 9, 1, 0, 0), new_duration=15,
+                                  new_link="https://www.youtube.com/watch?v=1q2w3e4r5t6y7u8i9o0p",
+                                  new_place="Sala 1",
+                                  new_responsible_professors_user_id=[repo.users[2].user_id, repo.users[11].user_id],
+                                  new_speakers=[
+                                      Speaker(
+                                          name="Fulano de Tal",
+                                          bio="Fulano de Tal é um professor de Engenharia de Software",
+                                          company="Universidade Federal de Fulano de tal",
+                                      )
+                                  ], new_total_slots=100,
+                                  new_accepting_new_enrollments=True,
+                                  new_stop_accepting_new_enrollments_before=None)
 
-       def test_update_activity_invalid_new_title(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
+    def test_update_activity_invalid_title(self):
+        repo = ActivityRepositoryMock()
+        usecase = UpdateActivityUsecase(repo)
 
-              with pytest.raises(EntityError):
-                     update_activity = usecase(code='ELET355', new_title=123)
+        with pytest.raises(EntityError):
+            update_activity = usecase(code=repo.activities[0].code, new_title=123, new_description='nova descricao',
+                                  new_activity_type=ACTIVITY_TYPE.LECTURES,
+                                  new_is_extensive=True,
+                                  new_delivery_model=DELIVERY_MODEL.ONLINE,
+                                  new_start_date=datetime.datetime(2021, 9, 1, 0, 0), new_duration=15,
+                                  new_link="https://www.youtube.com/watch?v=1q2w3e4r5t6y7u8i9o0p",
+                                  new_place="Sala 1",
+                                  new_responsible_professors_user_id=[repo.users[2].user_id, repo.users[11].user_id],
+                                  new_speakers=[
+                                      Speaker(
+                                          name="Fulano de Tal",
+                                          bio="Fulano de Tal é um professor de Engenharia de Software",
+                                          company="Universidade Federal de Fulano de tal",
+                                      )
+                                  ], new_total_slots=100,
+                                  new_accepting_new_enrollments=True,
+                                  new_stop_accepting_new_enrollments_before=None)
 
-       def test_update_activity_invalid_new_description(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
+    def test_update_activity_is_not_found(self):
+        repo = ActivityRepositoryMock()
+        usecase = UpdateActivityUsecase(repo)
 
-              with pytest.raises(EntityError):
-                     update_activity = usecase(code='ELET355', new_description=456)
+        with pytest.raises(NoItemsFound):
+            update_activity = usecase(code=repo.activities[0].code+"1", new_title="NOVO TITULO", new_description='nova descricao',
+                                  new_activity_type=ACTIVITY_TYPE.LECTURES,
+                                  new_is_extensive=True,
+                                  new_delivery_model=DELIVERY_MODEL.ONLINE,
+                                  new_start_date=datetime.datetime(2021, 9, 1, 0, 0), new_duration=15,
+                                  new_link="https://www.youtube.com/watch?v=1q2w3e4r5t6y7u8i9o0p",
+                                  new_place="Sala 1",
+                                  new_responsible_professors_user_id=[repo.users[2].user_id, repo.users[11].user_id],
+                                  new_speakers=[
+                                      Speaker(
+                                          name="Fulano de Tal",
+                                          bio="Fulano de Tal é um professor de Engenharia de Software",
+                                          company="Universidade Federal de Fulano de tal",
+                                      )
+                                  ], new_total_slots=100,
+                                  new_accepting_new_enrollments=True,
+                                  new_stop_accepting_new_enrollments_before=None)
 
-       def test_update_activity_invalid_new_activity_type(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
+    def test_update_activity_invalid_responsible_professors(self):
+        repo = ActivityRepositoryMock()
+        usecase = UpdateActivityUsecase(repo)
 
-              with pytest.raises(EntityError):
-                     update_activity = usecase(code='ELET355', new_activity_type='TYPE_ERROR')
+        with pytest.raises(EntityError):
+            update_activity = usecase(code=repo.activities[0].code, new_title="NOVO TITULO", new_description='nova descricao',
+                                  new_activity_type=ACTIVITY_TYPE.LECTURES,
+                                  new_is_extensive=True,
+                                  new_delivery_model=DELIVERY_MODEL.ONLINE,
+                                  new_start_date=datetime.datetime(2021, 9, 1, 0, 0), new_duration=15,
+                                  new_link="https://www.youtube.com/watch?v=1q2w3e4r5t6y7u8i9o0p",
+                                  new_place="Sala 1",
+                                  new_responsible_professors_user_id=[repo.users[2].user_id, repo.users[11].user_id, 999],
+                                  new_speakers=[
+                                      Speaker(
+                                          name="Fulano de Tal",
+                                          bio="Fulano de Tal é um professor de Engenharia de Software",
+                                          company="Universidade Federal de Fulano de tal",
+                                      )
+                                  ], new_total_slots=100,
+                                  new_accepting_new_enrollments=True,
+                                  new_stop_accepting_new_enrollments_before=None)
 
-       def test_update_activity_invalid_new_is_extensive(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
+    def test_update_activity_invalid_responsible_professors_not_found(self):
+        repo = ActivityRepositoryMock()
+        usecase = UpdateActivityUsecase(repo)
 
-              with pytest.raises(EntityError):
-                     update_activity = usecase(code='ELET355', new_is_extensive='NAO EH')
-
-       def test_update_activity_invalid_new_delivery_model(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
-
-              with pytest.raises(EntityError):
-                     update_activity = usecase(code='ELET355', new_delivery_model=000)
-
-       def test_update_activity_invalid_new_start_date(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
-
-              with pytest.raises(EntityError):
-                     update_activity = usecase(code='ELET355', new_start_date='AGORA')
-
-       def test_update_activity_invalid_new_duration(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
-
-              with pytest.raises(EntityError):
-                     update_activity = usecase(code='ELET355', new_duration='1500 minutos')
-
-       def test_update_activity_invalid_new_responsible_professors(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
-
-              with pytest.raises(EntityError):
-                     update_activity = usecase(code='ELET355', new_responsible_professors=repo.speakers[0])
-
-       def test_update_activity_invalid_new_speakers(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
-
-              with pytest.raises(EntityError):
-                     update_activity = usecase(code='ELET355', new_speakers=repo.users[5])
-
-       def test_update_activity_invalid_new_total_slots(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
-
-              with pytest.raises(EntityError):
-                     update_activity = usecase(code='ELET355', new_total_slots='500')
-
-       def test_update_activity_invalid_new_taken_slots(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
-
-              with pytest.raises(EntityError):
-                     update_activity = usecase(code='ELET355', new_taken_slots='1')
-       
-       def test_update_activity_invalid_new_accepting_new_enrollments(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
-
-              with pytest.raises(EntityError):
-                     update_activity = usecase(code='ELET355', new_accepting_new_enrollments='NAO')
-
-       def test_update_activity_invalid_new_stop_accepting_new_enrollments_before(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
-
-              with pytest.raises(EntityError):
-                     update_activity = usecase(code='ELET355', new_stop_accepting_new_enrollments_before='DEPOIS DE AMANHA')
-       
-       def test_update_activity_is_none(self):
-              repo = ActivityRepositoryMock()
-              usecase = UpdateActivityUsecase(repo)
-
-              with pytest.raises(NoItemsFound):
-                     update_activity = usecase(code='')
-       
+        with pytest.raises(NoItemsFound):
+            update_activity = usecase(code=repo.activities[0].code, new_title="NOVO TITULO", new_description='nova descricao',
+                                  new_activity_type=ACTIVITY_TYPE.LECTURES,
+                                  new_is_extensive=True,
+                                  new_delivery_model=DELIVERY_MODEL.ONLINE,
+                                  new_start_date=datetime.datetime(2021, 9, 1, 0, 0), new_duration=15,
+                                  new_link="https://www.youtube.com/watch?v=1q2w3e4r5t6y7u8i9o0p",
+                                  new_place="Sala 1",
+                                  new_responsible_professors_user_id=[repo.users[2].user_id, repo.users[11].user_id, "9999"],
+                                  new_speakers=[
+                                      Speaker(
+                                          name="Fulano de Tal",
+                                          bio="Fulano de Tal é um professor de Engenharia de Software",
+                                          company="Universidade Federal de Fulano de tal",
+                                      )
+                                  ], new_total_slots=100,
+                                  new_accepting_new_enrollments=True,
+                                  new_stop_accepting_new_enrollments_before=None)
