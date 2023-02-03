53ac4379be2351a674730a449955e3cf94bd52ff
Vitor Soller <vgsoller@gmail.com>
2023-02-02 21:24:40 -0300

    add from_dynamo and to_entity to enrollment_dynamo_dto.py

--- src/shared/infra/dto/enrollment_dynamo_dto.py
+++ src/shared/infra/dto/enrollment_dynamo_dto.py
@@ -1,3 +1,4 @@
+from src.shared.domain.entities.enrollment import Enrollment
 from src.shared.domain.enums.enrollment_state_enum import ENROLLMENT_STATE
 
 
@@ -15,6 +16,9 @@ class EnrollmentDynamoDTO:
 
     @staticmethod
     def from_entity(self):
+        """
+        Parse a Enrollment entity to a EnrollmentDynamoDTO
+        """
         return EnrollmentDynamoDTO(
             activity_code=self.activity_code,
             user_id=self.user_id,
@@ -22,14 +26,39 @@ class EnrollmentDynamoDTO:
             date_subscribed=self.date_subscribed
         )
 
-
     def to_dynamo(self):
+        """
+        Parse a EnrollmentDynamoDTO to a DynamoDB item
+        """
         return {
             "activity_code": self.activity_code,
             "user_id": self.user_id,
             "state": self.state.value,
             "date_subscribed": self.date_subscribed
         }
+
+    @staticmethod
+    def from_dynamo(dynamo_data: dict):
+        """
+        Parse a DynamoDB item to a EnrollmentDynamoDTO
+        """
+        return EnrollmentDynamoDTO(
+            activity_code=dynamo_data.get("activity_code"),
+            user_id=dynamo_data.get("user_id"),
+            state=ENROLLMENT_STATE(dynamo_data.get("state")),
+            date_subscribed=int(dynamo_data.get("date_subscribed"))
+        )
+
+    def to_entity(self):
+        """
+        Parse a EnrollmentDynamoDTO to a Enrollment entity
+        """
+        return Enrollment(
+            activity_code=self.activity_code,
+            user_id=self.user_id,
+            state=self.state,
+            date_subscribed=self.date_subscribed
+        )
 
     def __eq__(self, other):
         return self.activity_code == other.activity_code and self.user_id == other.user_id and self.state == other.state and self.date_subscribed == other.date_subscribed
--- tests/shared/infra/dto/test_enrollment_dynamo_dto.py
+++ tests/shared/infra/dto/test_enrollment_dynamo_dto.py
@@ -62,3 +62,59 @@ class Test_EnrollmentDynamoDTO:
         assert enrollemnt_dto == expected
 
 
+    def test_from_dynamo(self):
+        dynamo_data = {
+            "activity_code": "activity_code",
+            "user_id": "d61dbf66-a10f-11ed-a8fc-0242ac120002",
+            "state": "ENROLLED",
+            "date_subscribed": 1671229013000
+        }
+
+        enrollment_dto = EnrollmentDynamoDTO.from_dynamo(dynamo_data)
+
+        expected = EnrollmentDynamoDTO(
+            activity_code="activity_code",
+            user_id="d61dbf66-a10f-11ed-a8fc-0242ac120002",
+            state=ENROLLMENT_STATE.ENROLLED,
+            date_subscribed=1671229013000
+        )
+
+        assert enrollment_dto == expected
+
+    def test_to_entity(self):
+        enrollment_dto = EnrollmentDynamoDTO(
+            activity_code="activity_code",
+            user_id="d61dbf66-a10f-11ed-a8fc-0242ac120002",
+            state=ENROLLMENT_STATE.ENROLLED,
+            date_subscribed=1671229013000
+        )
+
+        enrollment = enrollment_dto.to_entity()
+
+        expected = Enrollment(
+            activity_code="activity_code",
+            user_id="d61dbf66-a10f-11ed-a8fc-0242ac120002",
+            state=ENROLLMENT_STATE.ENROLLED,
+            date_subscribed=1671229013000
+        )
+
+        assert enrollment == expected
+
+    def test_from_dynamo_to_entity(self):
+        dynamo_data = {
+            "activity_code": "activity_code",
+            "user_id": "d61dbf66-a10f-11ed-a8fc-0242ac120002",
+            "state": "ENROLLED",
+            "date_subscribed": 1671229013000
+        }
+
+        enrollment = EnrollmentDynamoDTO.from_dynamo(dynamo_data).to_entity()
+
+        expected = Enrollment(
+            activity_code="activity_code",
+            user_id="d61dbf66-a10f-11ed-a8fc-0242ac120002",
+            state=ENROLLMENT_STATE.ENROLLED,
+            date_subscribed=1671229013000
+        )
+
+        assert enrollment == expected
