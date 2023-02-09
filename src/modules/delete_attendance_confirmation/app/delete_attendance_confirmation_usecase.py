e89b325155e74a1df086f03f4e34c2f54c67ee14
LucasRomanato0 <20.00313-7@maua.br>
2023-02-09 20:04:30 -0300

    creating usecase route delete attendance confirmation

---
+++ src/modules/delete_attendance_confirmation/app/delete_attendance_confirmation_usecase.py
@@ -0,0 +1,49 @@
+from src.shared.domain.entities.user import User
+from src.shared.domain.enums.role_enum import ROLE
+from src.shared.domain.repositories.activity_repository_interface import IActivityRepository
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import NoItemsFound, ForbiddenAction
+
+class DeleteAttendanceConfirmationUsecase:
+       def __init__(self, repo: IActivityRepository):
+              self.repo = repo
+
+       def __call__(self, code: str, requester_user: User) -> str:
+              if type(code) != str:
+                     raise EntityError("activity_code")
+
+              if requester_user.role != ROLE.PROFESSOR:
+                     raise ForbiddenAction("user, not professor")
+
+              activity = self.repo.get_activity(code)
+
+              if activity is None:
+                     raise NoItemsFound("activity")
+
+              if activity.confirmation_code is None:
+                     raise ForbiddenAction("confirmation_code, do not exists")
+
+              if requester_user not in activity.responsible_professors:
+                     raise ForbiddenAction("user, not professor of activity")
+
+              delete_confirmation_code_activity = self.repo.update_activity(
+                     code=code,
+                     new_title=activity.title,
+                     new_description=activity.description,
+                     new_activity_type=activity.activity_type,
+                     new_is_extensive=activity.is_extensive,
+                     new_delivery_model=activity.delivery_model,
+                     new_start_date=activity.start_date,
+                     new_duration=activity.duration,
+                     new_link=activity.link,
+                     new_place=activity.place,
+                     new_responsible_professors=activity.responsible_professors,
+                     new_speakers=activity.speakers,
+                     new_total_slots=activity.total_slots,
+                     new_taken_slots=activity.taken_slots,
+                     new_accepting_new_enrollments=activity.accepting_new_enrollments,
+                     new_stop_accepting_new_enrollments_before=activity.stop_accepting_new_enrollments_before,
+                     new_confirmation_code='',
+              )
+
+              return delete_confirmation_code_activity
---
+++ tests/modules/delete_attendance_confirmation/app/test_delete_attendance_confirmation_usecase.py
@@ -0,0 +1,70 @@
+import pytest
+
+from src.modules.delete_attendance_confirmation.app.delete_attendance_confirmation_usecase import DeleteAttendanceConfirmationUsecase
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import ForbiddenAction, NoItemsFound
+from src.shared.infra.repositories.activity_repository_mock import ActivityRepositoryMock
+from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
+
+class Test_DeleteAttendanceConfirmationUsecase:
+       def test_delete_attendance_confirmation_code_usecase(self):
+              repo = ActivityRepositoryMock()
+              repo_user = UserRepositoryMock()
+              usecase = DeleteAttendanceConfirmationUsecase(repo)
+
+              requester_user = repo_user.users[2]
+
+              delete_confirmation_code = usecase(code=repo.activities[11].code, requester_user=requester_user)
+
+              assert repo.activities[11].confirmation_code == ''
+              assert delete_confirmation_code == repo.activities[11]
+
+       def test_delete_attendance_confirmation_code_usecase_wrong_code_type(self):
+              repo = ActivityRepositoryMock()
+              repo_user = UserRepositoryMock()
+              usecase = DeleteAttendanceConfirmationUsecase(repo)
+
+              requester_user = repo_user.users[2]
+
+              with pytest.raises(EntityError):
+                     usecase(code=123, requester_user=requester_user)
+
+       def test_delete_attendance_confirmation_code_usecase_wrong_role(self):
+              repo = ActivityRepositoryMock()
+              repo_user = UserRepositoryMock()
+              usecase = DeleteAttendanceConfirmationUsecase(repo)
+
+              requester_user = repo_user.users[0]
+
+              with pytest.raises(ForbiddenAction):
+                     usecase(code=repo.activities[0].code, requester_user=requester_user)
+
+       def test_delete_attendance_confirmation_code_usecase_wrong_user(self):
+              repo = ActivityRepositoryMock()
+              repo_user = UserRepositoryMock()
+              usecase = DeleteAttendanceConfirmationUsecase(repo)
+
+              requester_user = repo_user.users[0]
+
+              with pytest.raises(ForbiddenAction):
+                     usecase(code=repo.activities[11].code, requester_user=requester_user)
+
+       def test_delete_attendance_confirmation_code_activity_not_found(self):
+              repo = ActivityRepositoryMock()
+              repo_user = UserRepositoryMock()
+              usecase = DeleteAttendanceConfirmationUsecase(repo)
+
+              requester_user = repo_user.users[2]
+
+              with pytest.raises(NoItemsFound):
+                     usecase(code="UM_CODIGO_QUALQUER", requester_user=requester_user)
+
+       def test_delete_attendance_confirmation_code_activity_do_not_have_confirmation_code(self):
+              repo = ActivityRepositoryMock()
+              repo_user = UserRepositoryMock()
+              usecase = DeleteAttendanceConfirmationUsecase(repo)
+
+              requester_user = repo_user.users[2]
+
+              with pytest.raises(ForbiddenAction):
+                     usecase(code=repo.activities[0].code, requester_user=requester_user)  
