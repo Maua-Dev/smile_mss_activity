6d04b721a2b658265a52985b607ec7681002e25d
Briqz23 <dbriquez.devmaua@gmail.com>
2023-03-01 15:41:57 -0300

    import fix (removed unused forbidden action) + reordering

--- src/modules/enroll_activity/app/enroll_activity_usecase.py
+++ src/modules/enroll_activity/app/enroll_activity_usecase.py
@@ -5,7 +5,7 @@ from src.shared.domain.entities.user import User
 from src.shared.domain.enums.enrollment_state_enum import ENROLLMENT_STATE
 from src.shared.domain.repositories.activity_repository_interface import IActivityRepository
 from src.shared.helpers.errors.domain_errors import EntityError
-from src.shared.helpers.errors.usecase_errors import ForbiddenAction, NoItemsFound, ClosedActivity, \
+from src.shared.helpers.errors.usecase_errors import NoItemsFound, ClosedActivity, \
     UserAlreadyEnrolled
 
 
@@ -26,12 +26,11 @@ class EnrollActivityUsecase:
 
         enrollment = self.repo.get_enrollment(user_id=user_id, code=code)
 
+        if not activity.accepting_new_enrollments:
+            raise ClosedActivity("Activity")
 
         if enrollment is not None:
             raise UserAlreadyEnrolled('Enrollment')
-
-        if not activity.accepting_new_enrollments:
-            raise ClosedActivity("Activity")
 
         else:
 
