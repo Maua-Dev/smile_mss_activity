e9b121708abbf80ceab2da10287200ecd8d2cb4f
Vitor Soller <vgsoller@gmail.com>
2023-03-19 17:15:10 -0300

    add social name

--- src/shared/infra/dto/user_cognito_dto.py
+++ src/shared/infra/dto/user_cognito_dto.py
@@ -11,8 +11,10 @@ class UserCognitoDTO:
     phone: str
     accepted_notifications_sms: bool
     accepted_notifications_email: bool
+    social_name: str
+    certificate_with_social_name: bool
 
-    def __init__(self, name: str, role: ROLE, user_id: str, email: str = None, phone: str = None, accepted_notifications_sms: bool = None, accepted_notifications_email: bool = None):
+    def __init__(self, name: str, role: ROLE, user_id: str, email: str = None, phone: str = None, accepted_notifications_sms: bool = None, accepted_notifications_email: bool = None, social_name: str = None, certificate_with_social_name: bool = None):
         self.name = name
         self.role = role
         self.user_id = user_id
@@ -20,6 +22,8 @@ class UserCognitoDTO:
         self.phone = phone
         self.accepted_notifications_sms = accepted_notifications_sms
         self.accepted_notifications_email = accepted_notifications_email
+        self.social_name = social_name
+        self.certificate_with_social_name = certificate_with_social_name
 
     @staticmethod
     def from_cognito(cognito_user: dict):
@@ -34,7 +38,9 @@ class UserCognitoDTO:
             email=user_data["email"],
             phone=user_data.get("phone_number"),
             accepted_notifications_sms=eval(user_data["acceptedNotificSMS"].title()),
-            accepted_notifications_email=eval(user_data["acceptedNotificMail"].title())
+            accepted_notifications_email=eval(user_data["acceptedNotificMail"].title()),
+            social_name=user_data.get("socialName"),
+            certificate_with_social_name=eval(user_data["certWithSocialName"].title())
         )
 
     def to_entity(self):
@@ -52,7 +58,9 @@ class UserCognitoDTO:
             email=self.email,
             phone=self.phone,
             accepted_notifications_sms=self.accepted_notifications_sms,
-            accepted_notifications_email=self.accepted_notifications_email
+            accepted_notifications_email=self.accepted_notifications_email,
+            social_name=self.social_name,
+            certificate_with_social_name=self.certificate_with_social_name
         )
 
     def __eq__(self, other):
