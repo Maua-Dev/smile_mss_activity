4732c8fb349a2e0c446470c0040948bdfecd851a
Vitor Soller <vgsoller@gmail.com>
2023-03-22 21:10:08 -0300

    add to lambda_stack.py 🥕

--- iac/iac/lambda_stack.py
+++ iac/iac/lambda_stack.py
@@ -164,6 +164,14 @@ class LambdaStack(Construct):
             authorizer=authorizer
         )
 
+        self.enroll_activity_admin_function = self.create_lambda_api_gateway_integration(
+            module_name="enroll_activity_admin",
+            method="POST",
+            mss_student_api_resource=api_gateway_resource,
+            environment_variables=environment_variables,
+            authorizer=authorizer
+        )
+
         self.functions_that_need_dynamo_permissions = [
             self.enroll_activity_function,
             self.drop_activity_function,
@@ -180,7 +188,8 @@ class LambdaStack(Construct):
             self.delete_attendance_confirmation_function,
             self.manual_attendance_change_function,
             self.get_activity_with_enrollments_function,
-            self.manual_drop_activity_function
+            self.manual_drop_activity_function,
+            self.enroll_activity_admin_function
         ]
 
         self.functions_that_need_cognito_permissions = [
@@ -189,7 +198,8 @@ class LambdaStack(Construct):
             self.get_all_activities_admin_function,
             self.get_activity_with_enrollments_function,
             self.manual_attendance_change_function,
-            self.manual_drop_activity_function
+            self.manual_drop_activity_function,
+            self.enroll_activity_admin_function
         ]
 
 
