3062c09ce9c34ca4f98469ee572f994c89bdb8c8
Vitor Soller <vgsoller@gmail.com>
2023-03-24 23:38:11 -0300

    add certificate system

--- iac/app.py
+++ iac/app.py
@@ -4,6 +4,7 @@ import os
 import aws_cdk as cdk
 
 from adjust_layer_directory import adjust_layer_directory
+from iac.certificates_s3_stack import CertificatesS3Stack
 from iac.iac_stack import IacStack
 
 print("Starting the CDK")
@@ -19,6 +20,7 @@ aws_region = os.environ.get("AWS_REGION")
 aws_account_id = os.environ.get("AWS_ACCOUNT_ID")
 stack_name = os.environ.get("STACK_NAME")
 
+CertificatesS3Stack(app, f"{stack_name}-certificates-s3", env=cdk.Environment(account=aws_account_id, region=aws_region))
 IacStack(app, stack_name, env=cdk.Environment(account=aws_account_id, region=aws_region))
 
 app.synth()
