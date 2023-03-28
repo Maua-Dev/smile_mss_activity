b6941b20dfebb1a6b711d07345a7f8c8e31d96d6
Vitor Soller <vgsoller@gmail.com>
2023-03-28 19:31:06 -0300

    change message 😎

--- src/shared/helpers/utils/compose_enrolled_email.py
+++ src/shared/helpers/utils/compose_enrolled_email.py
@@ -283,7 +283,7 @@ def compose_enrolled_email(activity: Activity, user: UserInfo):
         <tbody>
         <tr>
         <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:25px 10px 50px;font-family:'Open Sans',sans-serif;" align="left">
-        <h1 class="v-font-size" style="margin: 0px; color: #ffffff; line-height: 140%; text-align: center; word-wrap: break-word; font-size: 22px;"><strong>Lembrete de Atividade</strong></h1>
+        <h1 class="v-font-size" style="margin: 0px; color: #ffffff; line-height: 140%; text-align: center; word-wrap: break-word; font-size: 22px;"><strong>Nova vaga disponível</strong></h1>
         </td>
         </tr>
         </tbody>
