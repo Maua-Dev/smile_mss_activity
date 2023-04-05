e73b0ed83964cc2ab397df9ff00a50c329dc2d24
Vitor Soller <vgsoller@gmail.com>
2023-04-05 17:41:39 -0300

    add typing_extensions

--- iac/iac/certificates_lambda_stack.py
+++ iac/iac/certificates_lambda_stack.py
@@ -34,13 +34,18 @@ class CertificatesLambdaStack(Construct):
                                                  compatible_runtimes=[lambda_.Runtime.PYTHON_3_9]
                                                  )
 
+        self.lambda_layer_typing_extensions = lambda_.LayerVersion(self, "typing_extensions_layer",
+                                                        code=lambda_.Code.from_asset("./lambda_requirements_layer_temp/typing_extensions"),
+                                                        compatible_runtimes=[lambda_.Runtime.PYTHON_3_9]
+                                                        )
+
 
         self.generate_certificate_function = lambda_.Function(
             self, "generate_certificate",
             code=lambda_.Code.from_asset(f"../certificates/generate_certificates"),
             handler=f"generate_certifificates.lambda_handler",
             runtime=lambda_.Runtime.PYTHON_3_9,
-            layers=[self.lambda_layer_activity, self.lambda_layer_PyPDF2, self.lambda_layer_reportlab,
+            layers=[self.lambda_layer_activity, self.lambda_layer_PyPDF2, self.lambda_layer_reportlab, self.lambda_layer_typing_extensions,
                     self.lambda_layer_pillow],
             environment=environment_variables,
             timeout=Duration.seconds(15),
