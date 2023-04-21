2f5362e5777088dd440baa1d4cd49b5114a3846e
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2023-04-21 14:04:38 -0300

    observability in get_enrollments_by_user and manual_attendance_change

--- src/modules/get_enrollments_by_user/app/get_enrollments_by_user_presenter.py
+++ src/modules/get_enrollments_by_user/app/get_enrollments_by_user_presenter.py
@@ -3,16 +3,26 @@ from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHt
 from .get_enrollments_by_user_controller import \
     GetEnrollmentsByUserController
 from .get_enrollments_by_user_usecase import GetEnrollmentsByUserUsecase
+observability = Environments.get_observability()(module_name="get_enrollments_by_user")
 
 repo = Environments.get_activity_repo()()
-usecase = GetEnrollmentsByUserUsecase(repo)
-controller = GetEnrollmentsByUserController(usecase)
+usecase = GetEnrollmentsByUserUsecase(repo, observability=observability)
+controller = GetEnrollmentsByUserController(usecase, observability=observability)
 
 
-def lambda_handler(event, context):
+@observability.presenter_decorators
+def get_enrollments_by_user_presenter(event, context):
     httpRequest = LambdaHttpRequest(data=event)
     httpRequest.data['requester_user'] = event.get('requestContext', {}).get('authorizer', {}).get('claims', None)
     response = controller(httpRequest)
     httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
 
     return httpResponse.toDict()
+
+@observability.handler_decorators
+def lambda_handler(event, context):
+    
+    response = get_enrollments_by_user_presenter(event, context)
+    observability.add_error_count_metric(response["statusCode"]) # ErrorCount metrics
+    
+    return response
--- src/modules/manual_attendance_change/app/manual_attendance_change_presenter.py
+++ src/modules/manual_attendance_change/app/manual_attendance_change_presenter.py
@@ -3,17 +3,27 @@ from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHt
 from .manual_attendance_change_controller import \
     ManualAttendanceChangeController
 from .manual_attendance_change_usecase import ManualAttendanceChangeUsecase
+observability = Environments.get_observability()(module_name="manual_attendance_change")
 
 repo_activity = Environments.get_activity_repo()()
 repo_user = Environments.get_user_repo()()
-usecase = ManualAttendanceChangeUsecase(repo_activity, repo_user)
-controller = ManualAttendanceChangeController(usecase)
+usecase = ManualAttendanceChangeUsecase(repo_activity, repo_user, observability=observability)
+controller = ManualAttendanceChangeController(usecase, observability=observability)
 
 
-def lambda_handler(event, context):
+@observability.presenter_decorators
+def manual_attendance_change_presenter(event, context):
     httpRequest = LambdaHttpRequest(data=event)
     httpRequest.data['requester_user'] = event.get('requestContext', {}).get('authorizer', {}).get('claims', None)
     response = controller(httpRequest)
     httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
 
     return httpResponse.toDict()
+
+@observability.handler_decorators
+def lambda_handler(event, context):
+    
+    response = manual_attendance_change_presenter(event, context)
+    observability.add_error_count_metric(response["statusCode"]) # ErrorCount metrics
+    
+    return response
