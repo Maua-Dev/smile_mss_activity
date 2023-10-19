18a62ca7cd9a07410a8636876f7be264c7ddd95b
MateusCMartins724 <mateus.c.martins@outlook.com>
2023-10-19 12:13:26 -0300

    fix create activity

--- tests/modules/create_activity/app/test_create_activity_controller.py
+++ tests/modules/create_activity/app/test_create_activity_controller.py
@@ -796,8 +796,6 @@ class Test_CreateActivityController:
                                     "delivery_model": "IN_PERSON",
                                     "start_date": 1669141012000,
                                     "end_date": 1671754613000,
-                                    "link": None,
-                                    "place": None,
                                     "responsible_professors": ["62cafdd4-a110-11ed-a8fc-0242ac120002", "03555624-a110-11ed-a8fc-0242ac120002"],
                                     "speakers": [{
                                         "name": "Robert Cecil Martin",
@@ -810,8 +808,7 @@ class Test_CreateActivityController:
         )
         response = controller(request=request)
 
-        assert response.status_code == 400
-        assert response.body == "Parâmetro inválido: link or place"
+        assert response.status_code == 201
 
     def test_create_activity_controller_online_no_link_established(self):
         repo_activity = ActivityRepositoryMock()
@@ -827,8 +824,6 @@ class Test_CreateActivityController:
                                     "delivery_model": "ONLINE",
                                     "start_date": 1669141012000,
                                     "end_date": 1671754613000,
-                                    "link": None,
-                                    "place": None,
                                     "responsible_professors": ["62cafdd4-a110-11ed-a8fc-0242ac120002",
                                                                "03555624-a110-11ed-a8fc-0242ac120002"],
                                     "speakers": [{
@@ -845,8 +840,7 @@ class Test_CreateActivityController:
                               )
         response = controller(request=request)
 
-        assert response.status_code == 400
-        assert response.body == "Parâmetro inválido: link or place"
+        assert response.status_code == 201
 
     def test_create_activity_controller_hybrid_no_link_established(self):
         repo_activity = ActivityRepositoryMock()
