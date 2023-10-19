69aca84ffee7b72bdf29311238e786ad320c617a
Briqz23 <dbriquez.devmaua@gmail.com>
2023-10-19 20:52:49 -0300

    removing minor sintax errors

--- iac/iac/nested_stack.py
+++ iac/iac/nested_stack.py
@@ -1,99 +1,76 @@
-from aws_cdk.aws_apigateway import IntegrationResponse, MethodResponse, IntegrationResponse, MethodResponse
-from constructs import Construct
-from aws_cdk import App, CfnOutput, NestedStack, NestedStackProps, Stack
-from aws_cdk.aws_apigateway import Deployment, Method, MockIntegration, PassthroughBehavior, RestApi, Stage
+from aws_cdk.aws_apigateway import IntegrationResponse, MethodResponse, MockIntegration, PassthroughBehavior
+from aws_cdk.core import App, CfnOutput, NestedStack, Stack
+from aws_cdk.aws_apigateway import Deployment, RestApi, Stage
 
-#
-# This file showcases how to split up a RestApi's Resources and Methods across nested stacks.
-#
-# The root stack 'RootStack' first defines a RestApi.
-# Two nested stacks BooksStack and PetsStack, create corresponding Resources '/books' and '/pets'.
-# They are then deployed to a 'prod' Stage via a third nested stack - DeployStack.
-#
-# To verify this worked, go to the APIGateway
-#
 
 class RootStack(Stack):
-    def __init__(self, scope):
-        super().__init__(scope, "integ-restapi-import-RootStack")
+    def __init__(self, scope, id):
+        super().__init__(scope, id)
 
-        rest_api = RestApi(self, "RestApi",
-            cloud_watch_role=True,
-            deploy=False
-        )
+        rest_api = RestApi(self, "RestApi", cloud_watch_role=True, deploy=False)
         rest_api.root.add_method("ANY")
 
-        certificate_db_stack = CertificateDBStack(self,
+        certificate_db_stack = CertificateDBStack(self, "CertificateDBStack",
             rest_api_id=rest_api.rest_api_id,
-            root_resource_id=rest_api.rest_api_root_resource_id
+            root_resource_id=rest_api.root.resource_id
         )
-        certificate_lambda_stack = CertificateLambdaStack(self,
+        certificate_lambda_stack = CertificateLambdaStack(self, "CertificateLambdaStack",
             rest_api_id=rest_api.rest_api_id,
-            root_resource_id=rest_api.rest_api_root_resource_id
+            root_resource_id=rest_api.root.resource_id
         )
-        DeployStack(self,
+        DeployStack(self, "DeployStack",
             rest_api_id=rest_api.rest_api_id,
-            methods=certificate_db_stack.methods.concat(certificate_lambda_stack.methods)
+            methods=certificate_db_stack.methods + certificate_lambda_stack.methods
         )
 
         CfnOutput(self, "SmileActivity2024Stackdev",
-            value=f"https://q05zkbsv0g.execute-api.sa-east-1.amazonaws.com/prod/")
+            value=f"https://{rest_api.rest_api_id}.execute-api.sa-east-1.amazonaws.com/prod/")
 
         CfnOutput(self, "SmileActivity2024Stackdev-certificates-s3",
             value=f"https://d1yjyfuur24yxm.cloudfront.net/")
-        
-#SmileActivity2024Stackdev
-class SmileLambdaStack(NestedStack):
 
-    def __init__(self, scope, *, restApiId, rootResourceId, parameters=None, timeout=None, notificationArns=None, removalPolicy=None, description=None):
-        super().__init__(scope, "integ-restapi-import-PetsStack", restApiId=restApiId, rootResourceId=rootResourceId, parameters=parameters, timeout=timeout, notificationArns=notificationArns, removalPolicy=removalPolicy, description=description)
 
-        api = RestApi.from_rest_api_attributes(self, "RestApi",
-            rest_api_id=rest_api_id,
-            root_resource_id=root_resource_id
-        )
+class CertificateLambdaStack(NestedStack):
+    def __init__(self, scope, id, *, rest_api_id, root_resource_id, parameters=None, timeout=None, notificationArns=None, removalPolicy=None, description=None):
+        super().__init__(scope, id, parameters=parameters, timeout=timeout, notificationArns=notificationArns, removalPolicy=removalPolicy, description=description)
 
-        method = api.root.add_resource("pets").add_method("GET", MockIntegration(
-            integration_responses=[IntegrationResponse(
-                status_code="200"
-            )],
+        api = RestApi.from_rest_api_id(self, "RestApi", rest_api_id, root_resource_id=root_resource_id)
+
+        method = api.root.add_resource("prod").add_method("GET", MockIntegration(
+            integration_responses=[IntegrationResponse(status_code="200")],
             passthrough_behavior=PassthroughBehavior.NEVER,
             request_templates={
-                "application/json": "{ "statusCode": 200 }"
+                "application/json": '{ "statusCode": 200 }'
             }
         ),
             method_responses=[MethodResponse(status_code="200")]
         )
 
-        self.methods.push(method)
+        self.methods = [method]
 
-class BooksStack(NestedStack):
 
-    def __init__(self, scope, *, restApiId, rootResourceId, parameters=None, timeout=None, notificationArns=None, removalPolicy=None, description=None):
-        super().__init__(scope, "integ-restapi-import-BooksStack", restApiId=restApiId, rootResourceId=rootResourceId, parameters=parameters, timeout=timeout, notificationArns=notificationArns, removalPolicy=removalPolicy, description=description)
+class CertificateDBStack(NestedStack):
+    def __init__(self, scope, id, *, rest_api_id, root_resource_id, parameters=None, timeout=None, notificationArns=None, removalPolicy=None, description=None):
+        super().__init__(scope, id, parameters=parameters, timeout=timeout, notificationArns=notificationArns, removalPolicy=removalPolicy, description=description)
 
-        api = RestApi.from_rest_api_attributes(self, "RestApi",
-            rest_api_id=rest_api_id,
-            root_resource_id=root_resource_id
-        )
+        api = RestApi.from_rest_api_id(self, "RestApi", rest_api_id, root_resource_id=root_resource_id)
 
-        method = api.root.add_resource("books").add_method("GET", MockIntegration(
-            integration_responses=[IntegrationResponse(
-                status_code="200"
-            )],
+        method = api.root.add_resource("").add_method("GET", MockIntegration(
+            integration_responses=[IntegrationResponse(status_code="200")],
             passthrough_behavior=PassthroughBehavior.NEVER,
             request_templates={
-                "application/json": "{ "statusCode": 200 }"
+                "application/json": '{ "statusCode": 200 }'
             }
         ),
             method_responses=[MethodResponse(status_code="200")]
         )
 
-        self.methods.push(method)
+        self.methods = [method]
+
 
 class DeployStack(NestedStack):
-    def __init__(self, scope, *, restApiId, methods=None, parameters=None, timeout=None, notificationArns=None, removalPolicy=None, description=None):
-        super().__init__(scope, "integ-restapi-import-DeployStack", restApiId=restApiId, methods=methods, parameters=parameters, timeout=timeout, notificationArns=notificationArns, removalPolicy=removalPolicy, description=description)
+    def __init__(self, scope, id, *, rest_api_id, methods=None, parameters=None, timeout=None, notificationArns=None, removalPolicy=None, description=None):
+        super().__init__(scope, id, parameters=parameters, timeout=timeout, notificationArns=notificationArns, removalPolicy=removalPolicy, description=description)
 
         deployment = Deployment(self, "Deployment",
             api=RestApi.from_rest_api_id(self, "RestApi", rest_api_id)
@@ -103,4 +80,7 @@ class DeployStack(NestedStack):
                 deployment.node.add_dependency(method)
         Stage(self, "Stage", deployment=deployment)
 
-RootStack(App())
+
+app = App()
+RootStack(app, "integ-restapi-import-RootStack")
+app.synth()
