8a6df0c5315ab330fcb671d21a9372570054451e
Briqz23 <dbriquez.devmaua@gmail.com>
2023-10-25 21:57:20 -0300

    sorry mateus

--- iac/app.py
+++ iac/app.py
@@ -7,7 +7,7 @@ import aws_cdk as cdk
 
 from adjust_layer_directory import adjust_layer_directory
 from iac.certificates_s3_stack import CertificatesS3Stack
-from iac.iac.nested_stack import RootStack
+from iac.nested_stack import RootStack
 from iac.iac_stack import IacStack
 from setup_requirements_layers import setup_requirements_layers
 
